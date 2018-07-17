97432feba733b30a0165661ba4c6b8088f34652a
Oksana Tkachenko <oksana@crystax.net>
2018-07-17 13:26:20 +0300

    changed css for nav-belt and added products.scss

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import 'bootstrap';
 import $ from 'jquery';
+//import lightSlider from 'lightslider';
+
 
 //import 'bootstrap/dist/css/bootstrap.min.css';
-
+/*
+$(document).ready(function() {
+  console.log('slider');
+  $("#lightSlider").lightSlider({
+    item: 3,
+  });
+});
+*/
